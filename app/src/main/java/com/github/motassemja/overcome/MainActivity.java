038a119039f194124d3cd4f668d5d9f89cea1bc7
MJ92 <m.j.92@live.com>
2018-03-31 14:10:10 +0300

    Add menu

--- app/src/main/java/com/github/motassemja/overcome/MainActivity.java
+++ app/src/main/java/com/github/motassemja/overcome/MainActivity.java
@@ -1,7 +1,9 @@
 package com.github.motassemja.overcome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_parent_control) {
+            // TODO handle parent control
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
